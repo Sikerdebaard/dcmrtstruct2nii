fd8ba56d9c3da6a609ceec6606da949cfafeb203
Mathis Ersted Rasmussen <mathiser@gmail.com>
2023-01-30 09:03:52 +0100

    Vectorize fork master (#3)
    * vectorized and numba
    
    * Inplements Vectorized coord transform, scaling of masks and numba for speed
    
    ---------
    
    Co-authored-by: Mathis Rasmussen <mathis@oncology.au.dk>

--- dcmrtstruct2nii/adapters/convert/rtstructcontour2mask.py
+++ dcmrtstruct2nii/adapters/convert/rtstructcontour2mask.py
@@ -2,6 +2,7 @@ import logging
 
 import SimpleITK as sitk
 import numpy as np
+from numba import njit
 from skimage import draw
 
 from dcmrtstruct2nii.exceptions import ContourOutOfBoundsException
@@ -14,55 +15,89 @@ def scale_information_tuple(information_tuple: tuple, xy_scaling_factor: int, ou
     else:
         information_tuple = np.array(information_tuple) / scale_array
 
-    return out_type(information_tuple[0]), out_type(information_tuple[1]), out_type(information_tuple[2])
+    return tuple([out_type(info) for info in information_tuple])
+
+
+@njit
+def _get_transform_matrix(spacing, direction):
+    """
+    this returns the basics needed to run _transform_physical_point_to_continuous_index
+    """
+    s = np.array(spacing)
+    d = np.array(direction).reshape(3, 3)
+    m_IndexToPhysicalPoint = np.multiply(d, s)
+    m_PhysicalPointToIndex = np.linalg.inv(m_IndexToPhysicalPoint)
+
+    return m_PhysicalPointToIndex
+
+
+@njit
+def xor_update_np_mask(np_mask, filled_poly, z, mask_foreground, mask_background):
+    overlay = np.logical_xor(np_mask[z, :, :], filled_poly)
+    np_mask[z, :, :] = overlay
+
+
+@njit
+def scale_xy_coordinates(pts, xy_scaling_factor):
+    pts[:, :2] *= xy_scaling_factor
+
+
+@njit
+def _transform_physical_point_to_continuous_index(coords, m_PhysicalPointToIndex, origin):
+    """
+    This method does the same as SimpleITK's TransformPhysicalPointToContinuousIndex, but in a vectorized fashion.
+    The implementation is based on ITK's code found in https://itk.org/Doxygen/html/itkImageBase_8h_source.html#l00497 and
+    https://discourse.itk.org/t/solved-transformindextophysicalpoint-manually/1031/2
+    """
+    if m_PhysicalPointToIndex is None:
+        raise Exception("Run set transform variables first!")
+
+    # pts_intermediary = np.subtract(coords, origins[:coords.shape[0]])
+    pts_intermediary = np.empty_like(coords)
+    pts_intermediary[:, 0] = coords[:, 0] - origin[0]
+    pts_intermediary[:, 1] = coords[:, 1] - origin[1]
+    pts_intermediary[:, 2] = coords[:, 2] - origin[2]
+
+    idxs = pts_intermediary @ m_PhysicalPointToIndex
+    return idxs
 
 
 class DcmPatientCoords2Mask:
-    def _poly2mask(self, coords_x, coords_y, shape, xy_scaling_factor):
-        coords_x = coords_x * xy_scaling_factor
-        coords_y = coords_y * xy_scaling_factor
-        mask = draw.polygon2mask(tuple(reversed(shape)), np.column_stack((coords_y, coords_x)))
+    def __init__(self):
+        self.m_PhysicalPointToIndex = None
+        self.origin = None
 
-        return mask
+    def convert(self, rtstruct_contours, dicom_image, mask_background, mask_foreground, xy_scaling_factor):
+        self.m_PhysicalPointToIndex = _get_transform_matrix(spacing=dicom_image.GetSpacing(),
+                                                            direction=dicom_image.GetDirection())
+        self.origin = dicom_image.GetOrigin()
 
-    def convert(self, rtstruct_contours, dicom_image, mask_background, mask_foreground, xy_scaling_factor=1):
         shape = scale_information_tuple(information_tuple=dicom_image.GetSize(), xy_scaling_factor=xy_scaling_factor, up=True, out_type=int)
-        spacing = scale_information_tuple(information_tuple=dicom_image.GetSpacing(), xy_scaling_factor=xy_scaling_factor, up=False, out_type=float)
 
-        mask = sitk.Image(*shape, sitk.sitkUInt8)
-        mask.SetSpacing(spacing)
-        mask.SetDirection(dicom_image.GetDirection())
-        mask.SetOrigin(dicom_image.GetOrigin())
-
-        np_mask = sitk.GetArrayFromImage(mask)
-        np_mask.fill(mask_background)
-
+        np_mask = np.empty(list(reversed(shape)))
         for contour in rtstruct_contours:
             if contour['type'].upper() not in ['CLOSED_PLANAR', 'INTERPOLATED_PLANAR']:
                 if 'name' in contour:
                     logging.info(f'Skipping contour {contour["name"]}, unsupported type: {contour["type"]}')
                 else:
                     logging.info(f'Skipping unnamed contour, unsupported type: {contour["type"]}')
-                continue
+                return
 
+            # Stack coordinate components to one array
             coordinates = contour['points']
+            coords = np.column_stack((coordinates["x"],
+                                      coordinates["y"],
+                                      coordinates["z"]))
 
-            pts = np.zeros([len(coordinates['x']), 3])
-
-            for index in range(0, len(coordinates['x'])):
-                # lets convert world coordinates to voxel coordinates
-                world_coords = dicom_image.TransformPhysicalPointToContinuousIndex((coordinates['x'][index], coordinates['y'][index], coordinates['z'][index]))
-                pts[index, 0] = world_coords[0]
-                pts[index, 1] = world_coords[1]
-                pts[index, 2] = world_coords[2]
-
+            # transform points to continous index
+            pts = _transform_physical_point_to_continuous_index(coords,
+                                                                m_PhysicalPointToIndex=self.m_PhysicalPointToIndex,
+                                                                origin=self.origin)
             z = int(round(pts[0, 2]))
-
             try:
-                filled_poly = self._poly2mask(pts[:, 0], pts[:, 1], [shape[0], shape[1]], xy_scaling_factor=xy_scaling_factor)
-                new_mask = np.logical_xor(np_mask[z, :, :], filled_poly)
-                np_mask[z, :, :] = np.where(new_mask, mask_foreground, mask_background)
-
+                scale_xy_coordinates(pts, xy_scaling_factor=xy_scaling_factor)
+                filled_poly = draw.polygon2mask((shape[1], shape[0]), pts[:, 1::-1])
+                xor_update_np_mask(np_mask=np_mask, filled_poly=filled_poly, z=z, mask_background=mask_background, mask_foreground=mask_foreground)
             except IndexError:
                 # if this is triggered the contour is out of bounds
                 raise ContourOutOfBoundsException()
@@ -71,16 +106,20 @@ class DcmPatientCoords2Mask:
                 if 'index out of bounds' in str(e):
                     raise ContourOutOfBoundsException()
                 raise e  # something serious is going on
+            except Exception as e:
+                print(e)
 
-        # Get image form final mask
-        final_mask = sitk.GetImageFromArray(np_mask)
+        # To get correct type
+        template_mask = sitk.Image(shape, sitk.sitkUInt8)
+        template_type = sitk.GetArrayFromImage(template_mask).dtype
 
-        # Adjusted spacing
-        spacing = scale_information_tuple(information_tuple=dicom_image.GetSpacing(), xy_scaling_factor=xy_scaling_factor, up=False, out_type=float)
-        final_mask.SetSpacing(spacing)
+        # np_mask to image
+        final_mask = sitk.GetImageFromArray(np_mask.astype(template_type))
 
-        # Original direction and origion
         final_mask.SetDirection(dicom_image.GetDirection())
         final_mask.SetOrigin(dicom_image.GetOrigin())
+
+        spacing = scale_information_tuple(information_tuple=dicom_image.GetSpacing(), xy_scaling_factor=xy_scaling_factor, up=False, out_type=float)
+        final_mask.SetSpacing(spacing)
 
         return final_mask
